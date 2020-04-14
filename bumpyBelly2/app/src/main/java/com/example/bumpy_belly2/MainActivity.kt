915eb90a8c8ca5c25f3089061d51e9da2da478d2
stevenbjones <steven.bjones@student.odisee.be>
2020-04-14 09:50:20 +0200

    Facts kunnen weergegeven worden

--- bumpyBelly2/app/src/main/java/com/example/bumpy_belly2/MainActivity.kt
+++ bumpyBelly2/app/src/main/java/com/example/bumpy_belly2/MainActivity.kt
@@ -13,7 +13,10 @@ package com.example.bumpy_belly2
     import com.firebase.ui.auth.AuthUI
     import com.firebase.ui.auth.IdpResponse
     import com.google.firebase.auth.FirebaseAuth
+    import com.google.firebase.auth.FirebaseUser
     import com.google.firebase.firestore.FirebaseFirestore
+    import com.google.firebase.firestore.Source
+
     import kotlinx.android.synthetic.main.activity_main.*
     import kotlinx.android.synthetic.main.fragment_home_page.*
     import java.util.*
@@ -62,26 +65,37 @@ class MainActivity : AppCompatActivity() {
                     val user = FirebaseAuth.getInstance().currentUser // get the current user
                     Toast.makeText(this, ""+user!!.email,Toast.LENGTH_SHORT).show()
 
-                  //fqsd
 
-                    //Maak hash om toe te kunnen voegen in database
-                    val testUser = hashMapOf(
-                        "UserId" to user.uid
-                    )
+                    //Functie om inglogde user toe te voegen aan database
+                    VoegUserToeAanDataBase(user)
 
-                    //Voeg het toe aan de database
-                    db.collection("Users").document(user.email.toString())
-                        .set(testUser)
-                        .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully written!") }
-                        .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
+                    //VoegFactsToeAanDataBase()
+
 
                     //weet niet of dit een goeie methode is om het te doen
                     nav_host_fragment.activity!!.setContentView(R.layout.fragment_home_page)
 
+                    val weetje = ""
+                    val docRef = db.collection("Facts").document("1")
+                    docRef.get()
+                        .addOnSuccessListener { document ->
+                            if (document != null) {
+                                Log.d(TAG, "DocumentSnapshot data: ${document.data}")
+
+                                findViewById<TextView>(R.id.TxtWeetjes).text = document.getString("Fact")
+
+                            } else {
+                                Log.d(TAG, "No such document")
+                            }
+                        }
+                        .addOnFailureListener { exception ->
+                            Log.d(TAG, "get failed with ", exception)
+                        }
 
                     //Het invullen van de weetjes
                     //Hier moet dus de weetjes van de dag ingevuld worden in plaats van "hier komen de weetjes"
-                     findViewById<TextView>(R.id.TxtWeetjes).text = "hier komen de weetjes"
+
+
 
 
                 }
@@ -92,7 +106,9 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        fun signout(){
+
+
+    fun signout(){
             sign_out.setOnClickListener {
                 //signout
                 AuthUI.getInstance().signOut(this@MainActivity)
@@ -116,4 +132,62 @@ class MainActivity : AppCompatActivity() {
                 .build(), MY_REQUEST_CODE
             )
         }
+
+        //Functie om facts toe te voegen aan database
+        fun VoegFactsToeAanDataBase(){
+
+            val Facts = arrayOf("Huilen in de baarmoeder. Baby’s oefenen in de baarmoeder al met huilen. Dit gebeurt meestal na ongeveer 28 weken zwangerschap. Ook is hij/zij in de buik al druk bezig met duimzuigen, schoppen en slikken.",
+                "Eerste beweging. Vanaf 8 weken zwangerschap begint je baby al met bewegen. Deze bewegingen voel je nog niet, want je baby is dan nog maar zo groot als een bosbes.",
+                " Aanmaak van urine. Vanaf de 13e week van je zwangerschap maakt je baby urine aan. Deze plast hij uit in het vruchtwater en drinkt hij vervolgens weer op. Maak je geen zorgen, dit kan geen kwaad.",
+                "Smaken proeven in de baarmoeder. Je baby kan al verschillende smaken proeven wanneer hij nog in de baarmoeder zit. Wanneer jij verschillende dingen eet, gaat je vruchtwater ook andere smaken produceren. En daar geniet je baby lekker van!",
+                " Geur. Na de geboorte gebruikt je baby de geur van jouw borst om de tepel te vinden. Na de bevalling scheiden je borsten een bepaalde geur af, dit helpt je baby om de borst te vinden en te drinken.",
+                "255 baby’s per minuut. Wereldwijd worden er per minuut 255 baby’s geboren. Tijdens jouw bevalling puffen er dus heel wat dames mee.",
+                "Een baby heeft ongeveer 10.000 smaakpapillen. Niet alleen op de tong maar ook aan de binnenkant van de wangen en het gehemelte.",
+                "Een baby herkent direct na de geboorte de geur en de stem van zijn moeder. Het duurt een paar weken voordat hij het verschil kan zien tussen de moeder en iemand anders.",
+                "Het eerste lachje komt zo tussen de 4 en 6 weken na de geboorte.",
+                "Op het moment dat een baby wordt geboren heeft het een hartslag 180 slagen per minuut. Na een paar uur is dit gedaald naar 140 slagen. Als het kind 1 jaar is, is dit ongeveer 115 slagen per minuut.",
+                "Tot de leeftijd van ongeveer 7 maanden kan een baby ademhalen en slikken tegelijk.",
+                "De meeste baby’s verliezen al het haar waar mee ze geboren worden in de eerste vier maanden van hun leven.",
+                "De eerste ontlasting van de future baby is al aanwezig vanaf 21 weken De ontlasting, ook wel ‘meconium’ genoemd, komt er pas uit na de geboorte. Voor de moeders in spé: schrik niet, het is zwart en plakt als een gek!",
+                "1 op de 2000 baby’s wordt geboren met een tand. In veel gevallen moet die dan wel verwijderd worden, omdat zo’n tand vaak een onvolgroeide wortel heeft.",
+                "De vingerafdruk van een baby wordt al bepaald in de eerste drie maanden van de zwangerschap.",
+                "Je baby heeft al na 6 weken zwangerschap een eigen hartslag.",
+                "Vanaf 23 weken zwangerschap kan je kindje schrikken van hard of onverwacht geluid.",
+                "Je voelt het zelf op dat moment nog niet, maar je kindje reageert al na 10 weken als je zachtjes op je buik duwt.",
+                "Vanaf 28 weken kan een baby dezelfde dingen ruiken die jij als moeder ruikt.",
+                "De schoenmaat van een zwangere vrouw kan een hele maat groter worden door het vocht dat ze vasthoudt.",
+                "Naweeën vinden een half uur na de bevalling plaats en kunnen soms wel 15 dagen duren. Ze zorgen er voor dat de baarmoeder weer op zijn plaats komt. Gewone pijnstillers kunnen de pijn verhelpen.",
+                "Een verhoogde kans op een tweeling als er tweelingen in de directe familie voorkomen, vooral als er tweelingen zijn aan moeders kant",
+                "Maak de fopspeen van je kindje vast aan een ballon en vertel je kleintje dat het op ontdekkingsreis gaat in de ruimte."
+            )
+
+            var x = 0
+            while(x<Facts.count()){
+
+                val Fact = hashMapOf(
+                    "Fact" to Facts[x]
+                )
+
+                db.collection("Facts").document("$x")
+                    .set(Fact)
+                    .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully written!") }
+                    .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
+                x++
+            }
+
+        }
+
+        fun VoegUserToeAanDataBase(user: FirebaseUser) {
+            //Maak hash om toe te kunnen voegen in database
+            val testUser = hashMapOf(
+                "UserId" to user.uid
+            )
+
+            //Voeg het toe aan de database
+            db.collection("Users").document(user.email.toString())
+                .set(testUser)
+                .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully written!") }
+                .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
+        }
+
     }
