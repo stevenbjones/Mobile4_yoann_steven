6d379d12030e2f6631e31b199b26d7233e9416f2
yoannrobyn <yoann.robyn@student.odisee.be>
2020-05-15 14:21:54 +0200

    Test versie voor change
    Nu wordt er steeds een nieuwe pregnancy aangemaakt als je op save drukt.

--- bumpyBelly2/app/src/main/java/com/example/bumpy_belly2/Change_pregnancy.kt
+++ bumpyBelly2/app/src/main/java/com/example/bumpy_belly2/Change_pregnancy.kt
@@ -10,6 +10,8 @@ import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_change_pregnancy.*
 import kotlinx.android.synthetic.main.fragment_change_pregnancy.view.*
 
@@ -58,11 +60,41 @@ class Change_pregnancy : Fragment() {
             }
         }
 
-
         view.btnSave.setOnClickListener {
            if(!checkBoxCancel.isChecked)
            {
-               // Als de checkBox "cancel" niet gechecked is zou hij de aanpassingen moeten uitvoeren.
+               val user = FirebaseAuth.getInstance().currentUser
+               val db = FirebaseFirestore.getInstance()
+
+               val Zwangerschap = hashMapOf(
+                   "Actief" to true,
+                   "StartDate" to if(datePicker.dayOfMonth.toString().length == 1 && datePicker.month.toString().length == 1) {
+                       ("0${datePicker.dayOfMonth} 0${datePicker.month + 1} ${datePicker.year}")
+                   }
+                   else if (datePicker.dayOfMonth.toString().length == 1 && datePicker.month.toString().length == 2 ){
+                       ("0${datePicker.dayOfMonth} ${datePicker.month + 1} ${datePicker.year}")
+                   }
+                   else if (datePicker.dayOfMonth.toString().length == 2 && datePicker.month.toString().length == 1 ){
+                       ("${datePicker.dayOfMonth} 0${datePicker.month + 1} ${datePicker.year}")
+                   }
+                   else{
+                       ("${datePicker.dayOfMonth} ${datePicker.month + 1} ${datePicker.year}")
+                   }
+               )
+
+               val SpecificatieChildren = hashMapOf(
+                   "Gender" to optie,
+                   "Name" to txtName.text.toString()
+               )
+
+               // Referenctie naar de pregnacies van user
+               var DBPregnanties =  db.collection("Users").document(user?.uid.toString()).collection("Pregnanties").document()
+               DBPregnanties.set(Zwangerschap)
+
+               //SubColletie van de User zijn pregnacie. Deze noemt children
+               DBPregnanties.collection("Children").document()
+                   .set( SpecificatieChildren)
+
 
 
                navController!!.navigate(R.id.action_change_pregnancy_to_homePage)
